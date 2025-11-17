335b678baa4a3984a0531aee27b2247b0ac32552
qasim-dev-12 <staytopdev@gmail.com>
2025-11-17 05:11:05 -0800

    images path updated

--- src/components/sections/Home1/Service.tsx
+++ src/components/sections/Home1/Service.tsx
@@ -63,8 +63,8 @@ function Service() {
                         <div className="inner-box">
                           <div className="image-box">
                             <figure className="image">
-                              <img src="assets/images/resource/01 Digital Marketing.png" alt="Image" />
-                              <img src="assets/images/resource/01 Digital Marketing.png" alt="Image" />
+                              <img src="assets/images/resource/04 PPC Services.png" alt="Image" />
+                              <img src="assets/images/resource/04 PPC Services.png" alt="Image" />
                             </figure>
                             <div className="icon-box">
                               <img className="icon-img" src="assets/images/icons/theme-icon1.png" alt="icon" />
@@ -82,8 +82,8 @@ function Service() {
                         <div className="inner-box">
                           <div className="image-box">
                             <figure className="image">
-                              <img src="assets/images/resource/02 Social Media Marketing.png" alt="Image" />
-                              <img src="assets/images/resource/02 Social Media Marketing.png" alt="Image" />
+                              <img src="assets/images/resource/06 Web Development.png" alt="Image" />
+                              <img src="assets/images/resource/06 Web Development.png" alt="Image" />
                             </figure>
                             <div className="icon-box">
                               <img className="icon-img" src="assets/images/icons/theme-icon2.png" alt="icon" />
@@ -102,8 +102,8 @@ function Service() {
                         <div className="inner-box">
                           <div className="image-box">
                             <figure className="image">
-                              <img src="assets/images/resource/03 SEO Services.png" alt="Image" />
-                              <img src="assets/images/resource/03 SEO Services.png" alt="Image" />
+                              <img src="assets/images/resource/01 Digital Marketing.png" alt="Image" />
+                              <img src="assets/images/resource/01 Digital Marketing.png" alt="Image" />
                             </figure>
                             <div className="icon-box">
                               <img className="icon-img" src="assets/images/icons/theme-icon3.png" alt="icon" />
@@ -121,8 +121,9 @@ function Service() {
                         <div className="inner-box">
                           <div className="image-box">
                             <figure className="image">
-                              <img src="assets/images/resource/04 PPC Services.png" alt="Image" />
-                              <img src="assets/images/resource/04 PPC Services.png" alt="Image" />
+                              
+                              <img src="assets\images\resource\02 Social Media Marketing.png" alt="Image" />
+                              <img src="assets\images\resource\02 Social Media Marketing.png" alt="Image" />
                             </figure>
                             <div className="icon-box">
                               <img className="icon-img" src="assets/images/icons/theme-icon3.png" alt="icon" />
@@ -140,8 +141,8 @@ function Service() {
                         <div className="inner-box">
                           <div className="image-box">
                             <figure className="image">
-                              <img src="assets/images/resource/05 Email Marketing.png" alt="Image" />
-                              <img src="assets/images/resource/05 Email Marketing.png" alt="Image" />
+                              <img src="assets/images/resource/03 SEO Services.png" alt="Image" />
+                              <img src="assets/images/resource/03 SEO Services.png" alt="Image" />
                             </figure>
                             <div className="icon-box">
                               <img className="icon-img" src="assets/images/icons/theme-icon3.png" alt="icon" />
@@ -159,8 +160,8 @@ function Service() {
                         <div className="inner-box">
                           <div className="image-box">
                             <figure className="image">
-                              <img src="assets/images/resource/06 Web Development.png" alt="Image" />
-                              <img src="assets/images/resource/06 Web Development.png" alt="Image" />
+                              <img src="/assets/images/resource/05 Email Marketing.png" alt="Image" />
+                              <img src="/assets/images/resource/05 Email Marketing.png" alt="Image" />
                             </figure>
                             <div className="icon-box">
                               <img className="icon-img" src="assets/images/icons/theme-icon3.png" alt="icon" />
