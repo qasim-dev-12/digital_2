1c5e397dcb1852c9c04de7e2a3b7087682f60875
qasim-dev-12 <staytopdev@gmail.com>
2025-11-26 01:38:23 -0800

    fixes done

--- src/components/sections/ProjectDetails.tsx
+++ src/components/sections/ProjectDetails.tsx
@@ -75,19 +75,19 @@ const ProjectDetails: React.FC<ProjectDetailsProps> = ({industry}) => {
           <ul className="list-unstyled project-details__details-list">
             <li>
               <p className="project-details__client">Industry Focus</p>
-              <h6 className="project-details__name leading-6 !w-[72px] overflow-hidden break-words w-full min-w-0">{industry?.industryInfo?.industryFocus}</h6>
+              <h6 className="project-details__name">{industry?.industryInfo?.industryFocus}</h6>
             </li>
             <li>
               <p className="project-details__client">Service Category</p>
-              <h6 className="project-details__name leading-6 !w-[72px] overflow-hidden break-words w-full min-w-0">{industry?.industryInfo?.serviceCategory}</h6>
+              <h6 className="project-details__name">{industry?.industryInfo?.serviceCategory}</h6>
             </li>
             <li>
               <p className="project-details__client">Region</p>
-              <h6 className="project-details__name leading-6 !w-[72px] overflow-hidden break-words w-full min-w-0">{industry?.industryInfo?.region}</h6>
+              <h6 className="project-details__name">{industry?.industryInfo?.region}</h6>
             </li>
             <li>
               <p className="project-details__client"> Objective</p>
-              <h6 className="project-details__name leading-6 !w-[72px] overflow-hidden break-words w-full min-w-0">{industry?.industryInfo?.objective}</h6>
+              <h6 className="project-details__name">{industry?.industryInfo?.objective}</h6>
             </li>
           </ul>
         </div>
